5b56ed9c000f441717c3304aaec5b56933f412a4
Pavel <elemir2010@gmail.com>
2024-02-21 23:27:09 +0300

    add Hometask4

---
+++ HomeTask4/HomeTask1_No_Storyboard/FriendsModel.swift
@@ -0,0 +1,28 @@
+//
+//  FriendsModel.swift
+//  HomeTask1_No_Storyboard
+//
+//  Created by Павел Яновский on 16.02.2024.
+//
+
+struct FriendsModel: Decodable {
+    let response: FriendsItems
+}
+
+struct FriendsItems: Decodable {
+    let items: [Friend]
+}
+
+struct Friend: Decodable {
+    let firstName: String
+    let lastName: String
+    let photo: String
+    let online: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case firstName = "first_name"
+        case lastName = "last_name"
+        case photo = "photo_50"
+        case online
+    }
+}
---
+++ HomeTask4/HomeTask1_No_Storyboard/TabBarController .swift
@@ -0,0 +1,26 @@
+//
+//  TabBarController .swift
+//  HomeTask1_No_Storyboard
+//
+//  Created by Павел Яновский on 12.02.2024.
+//
+
+//import Foundation
+//import UIKit
+//
+//class TabBarController: UITabBarController {
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        let friendsVC = FriendsViewController()
+//        friendsVC.title = "Friends"
+//
+//        let groupsVC = GroupsViewController()
+//        groupsVC.title = "Groups"
+//
+//        let photosVC = PhotosViewController()
+//        photosVC.title = "Photos"
+//
+//        viewControllers = [friendsVC, groupsVC, photosVC]
+//    }
+//}
